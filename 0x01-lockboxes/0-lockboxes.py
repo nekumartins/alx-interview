2eab1934a18a9db0574f96848a570c5e980ca2af
Chukwuneku <akpotohwoo@gmail.com>
2024-08-16 00:42:27 +0100

    fix pycodestyle

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """Solves the lock boxes puzzle """
 
+
 def canUnlockAll(boxes):
     opened_boxes = set()
     stack = [0]
-    
+
     while stack:
         current_box = stack.pop()
         if current_box not in opened_boxes:
@@ -12,5 +13,5 @@ def canUnlockAll(boxes):
             for key in boxes[current_box]:
                 if key not in opened_boxes and key < len(boxes):
                     stack.append(key)
-    
+
     return len(opened_boxes) == len(boxes)
