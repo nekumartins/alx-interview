17db71841efee4b091a4cdf18d1b368a303d9ae7
Chukwuneku <akpotohwoo@gmail.com>
2024-09-27 05:56:46 +0100

    init commit

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''2D matrix module'''
+
+
+def rotate_2d_matrix(matrix):
+    '''r
+    otates a 2d matrix 90° clockwise
+    Returns: None
+    '''
+    left, right = 0, len(matrix) - 1
+
+    while left < right:
+        for i in range(right - left):
+            top, bottom = left, right
+            topLeft = matrix[top][left + i]
+            matrix[top][left + i] = matrix[bottom - i][left]
+            matrix[bottom - i][left] = matrix[bottom][right - i]
+            matrix[bottom][right - i] = matrix[top + i][right]
+            matrix[top + i][right] = topLeft
+        right -= 1
+        left += 1
