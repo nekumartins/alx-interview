5ec808173a89557a70635a93968d9bc966f1d01d
Chukwuneku <akpotohwoo@gmail.com>
2024-10-04 05:21:11 +0100

    task complete

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''Givencoins of different values
+'''
+import sys
+
+
+def makeChange(coins, total):
+    '''
+    Return: fewest number of coins
+    '''
+    if total <= 0:
+        return 0
+    table = [sys.maxsize for i in range(total + 1)]
+    table[0] = 0
+    m = len(coins)
+    for i in range(1, total + 1):
+        for j in range(m):
+            if coins[j] <= i:
+                subres = table[i - coins[j]]
+                if subres != sys.maxsize and subres + 1 < table[i]:
+                    table[i] = subres + 1
+
+    if table[total] == sys.maxsize:
+        return -1
+    return table[total]
